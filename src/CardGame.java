0fd1be08793c93aa0f46fd2a19780b184580196a
Sebastian Larsson <sebastian.larsson@cybercom.com>
2017-10-05 14:14:58 +0200

    step 16

--- src/CardGame.java
+++ src/CardGame.java
@@ -7,20 +7,23 @@ public class CardGame {
 		for (int i = 0; i < cs2.length; i++) {
 			int p1Card = valueOf(cs[i]);
 			int p2Card = valueOf(cs2[i]);
-			if(p1Card > p2Card)
+			if (p1Card > p2Card)
 				p1Wins++;
-			if(p1Card < p2Card)
+			if (p1Card < p2Card)
 				p2Wins++;
 		}
-		
-		if(p1Wins > p2Wins)
+		return determineWinner(p1Wins, p2Wins);
+	}
+
+	private String determineWinner(int p1Wins, int p2Wins) {
+		if (p1Wins > p2Wins)
 			return "p1 wins " + p1Wins + " to " + p2Wins;
-		if(p1Wins < p2Wins)
+		if (p1Wins < p2Wins)
 			return "p2 wins " + p2Wins + " to " + p1Wins;
 		return "draw";
 	}
 
 	private int valueOf(char c) {
-		return new String(new char[] {'2','3','4','5','6','7','8','9','T','J','Q','K','A'}).indexOf(c);
+		return new String(new char[] { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' }).indexOf(c);
 	}
 }
--- src/CardGameTest.java
+++ src/CardGameTest.java
@@ -1,34 +1,38 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CardGameTest {
+	private CardGame cardgame;
+
 	private char[] hand(char... values) {
 		return values;
 	}
 
+	@Before
+	public void create() {
+		cardgame = new CardGame();
+	}
+	
 	@Test
 	public void draw() throws Exception {
-		CardGame cardgame = new CardGame();
 		assertEquals("draw", cardgame.play(hand('2'), hand('2')));
 	}
 
 	@Test
 	public void faced_card() {
-		CardGame cardgame = new CardGame();
 		assertEquals("p1 wins 1 to 0", cardgame.play(hand('J'), hand('T')));
 	}
 	
 	@Test
 	public void single_card() {
-		CardGame cardgame = new CardGame();
 		assertEquals("p1 wins 1 to 0", cardgame.play(hand('3'), hand('2')));
 		assertEquals("p2 wins 1 to 0", cardgame.play(hand('2'), hand('3')));
 	}
 
 	@Test
 	public void multiple_cards() {
-		CardGame cardgame = new CardGame();
 		assertEquals("p1 wins 2 to 0", cardgame.play(hand('3','3'), hand('2','2')));
 		assertEquals("p1 wins 2 to 1", cardgame.play(hand('3','3','2'), hand('2','2','3')));
 		assertEquals("p2 wins 2 to 1", cardgame.play(hand('2','2','3'), hand('3','3','2')));
