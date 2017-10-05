112ed0c8b8b10efec56ea673e572af77de970120
Sebastian Larsson <sebastian.larsson@cybercom.com>
2017-10-05 14:08:02 +0200

    step 11

--- src/CardGame.java
+++ src/CardGame.java
@@ -3,13 +3,16 @@ public class CardGame {
 
 	public String play(char[] cs, char[] cs2) {
 		int p1Wins = 0;
+		int p2Wins = 0;
 		for (int i = 0; i < cs2.length; i++) {
 			if(cs[i] > cs2[i])
 				p1Wins++;
+			if(cs[i] < cs2[i])
+				p2Wins++;
 		}
 		
 		if(p1Wins > 0)
-			return "p1 wins " + p1Wins + " to 0";
+			return "p1 wins " + p1Wins + " to " + p2Wins;
 		if(cs[0] < cs2[0])
 			return "p2 wins 1 to 0";
 		return "draw";
