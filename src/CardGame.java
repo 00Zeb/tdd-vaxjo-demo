78b299ec5481ee566d757ffbe8df754222a3cc6e
Sebastian Larsson <sebastian.larsson@cybercom.com>
2017-10-05 14:02:53 +0200

    step 7

--- src/CardGame.java
+++ src/CardGame.java
@@ -4,6 +4,8 @@ public class CardGame {
 	public String play(char[] cs, char[] cs2) {
 		if(cs[0] > cs2[0])
 			return "p1 wins 1 to 0";
+		if(cs[0] < cs2[0])
+			return "p2 wins 1 to 0";
 		return "draw";
 	}
 
