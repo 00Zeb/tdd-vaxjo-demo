cdd303c099e0c1c8494dd68aec5aa9fb06a5b82e
Sebastian Larsson <sebastian.larsson@cybercom.com>
2017-10-05 14:05:05 +0200

    step 8

--- src/CardGameTest.java
+++ src/CardGameTest.java
@@ -3,8 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CardGameTest {
-	private char[] hand(char value) {
-		return new char[] {value};
+	private char[] hand(char... values) {
+		return values;
 	}
 
 	@Test
@@ -18,5 +18,11 @@ public class CardGameTest {
 		CardGame cardgame = new CardGame();
 		assertEquals("p1 wins 1 to 0", cardgame.play(hand('3'), hand('2')));
 		assertEquals("p2 wins 1 to 0", cardgame.play(hand('2'), hand('3')));
+	}
+
+	@Test
+	public void multiple_cards() {
+		CardGame cardgame = new CardGame();
+		assertEquals("p1 wins 2 to 0", cardgame.play(hand('3','3'), hand('2','2')));
 	}
 }
